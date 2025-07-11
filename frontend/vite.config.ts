b2ab1964743b2d6c00f3f8aa719e583396b59da4
rublev <r@rublev.io>
2025-07-11 16:00:12 -0400

    - Update tailwind packages to their latest and integrate tailwind - Use scss instead of plain css - [Taskmaster] Add root tasks - Update all frontend packages to their latest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/postcss'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,11 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
